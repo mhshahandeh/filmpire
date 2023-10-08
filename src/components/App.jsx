89d2160d99640840d6c3d813be09c314b5d5f57a
mhshahandeh <shahandeh30@gmail.com>
2023-10-08 16:21:10 +0330

    styling methode changed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,37 @@
 import React from 'react';
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, styled } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import { Actors, MovieInformation, Movies, NavBar, Profile } from './index';
-import useStyles from './styles';
+
+const Container = styled('div')({
+  display: 'flex',
+  height: '100%',
+});
+
+const Main = styled('main')({
+  flexGrow: '1',
+  padding: '2em',
+});
+
+const Toolbar = styled('div')({
+  height: '70px',
+});
 
 function App() {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
+    <Container>
       <CssBaseline />
       <NavBar />
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
+      <Main>
+        <Toolbar />
         <Routes>
           <Route path="/" element={<Movies />} />
           <Route path="/movie/:id" element={<MovieInformation />} />
           <Route path="/actors/:id" element={<Actors />} />
           <Route path="/profile/:id" element={<Profile />} />
         </Routes>
-      </main>
-    </div>
+      </Main>
+    </Container>
   );
 }
 
