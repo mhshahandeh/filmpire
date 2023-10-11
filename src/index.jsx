2f1997fe5598eb1a64677db0f92e57fe4e515367
mhshahandeh <shahandeh30@gmail.com>
2023-10-11 17:49:21 +0330

    Redux Toolkit folder structure and api request

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import store from './app/store';
 
 const theme = createTheme({});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>,
 );
