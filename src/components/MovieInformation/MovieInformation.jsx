59473e9e67d3829dd65edf822067cf32d7f14b62
mhshahandeh <shahandeh30@gmail.com>
2023-10-18 15:19:21 +0330

    Movie information page completed

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,8 +1,199 @@
 import React from 'react';
+import { Modal, Typography, Button, ButtonGroup, Grid, Box, CircularProgress, useMediaQuery, Rating, styled } from '@mui/material';
+import { Movie as MovieIcon, Theaters, Language, PlusOne, Favorite, FavoriteBorderOutlined, Remove, ArrowBack } from '@mui/icons-material';
+import { Link, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import { useGetMovieQuery } from '../../services/TMDB';
+import genreIcons from '../../assets/genres';
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
+
+const GridContainerSpaceRound = styled(Grid)(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'space-around',
+  margin: '10px 0 !important',
+  [theme.breakpoints.down('sm')]: {
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+  },
+}));
+
+const Poster = styled('img')(({ theme }) => ({
+  borderRadius: '20px',
+  boxShadow: '0.5em 1em 1em rgb(64, 64, 70)',
+  width: '80%',
+  [theme.breakpoints.down('md')]: {
+    margin: '0 auto',
+    width: '50%',
+  },
+  [theme.breakpoints.down('sm')]: {
+    width: '100%',
+    height: '350px',
+    margin: '0 auto',
+    marginBottom: '30px',
+  },
+}));
+
+const GenresContainer = styled(Grid)(({
+  margin: '10px 0 !important',
+  display: 'flex',
+  justifyContent: 'space-around',
+  flexWrap: 'wrap',
+}));
+
+const Links = styled(Link)(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  textDecoration: 'none',
+  [theme.breakpoints.down('sm')]: {
+    padding: '0.5rem 1rem',
+  },
+}));
+
+const GenreImage = styled('img')(({ theme }) => ({
+  filter: theme.palette.mode === 'dark' && 'invert(1)',
+  marginRight: '10px',
+}));
+
+const CastImage = styled('img')(({
+  width: '100%',
+  maxWidth: '7em',
+  height: '8em',
+  objectFit: 'cover',
+  borderRadius: '10px',
+}));
+
+const ButtonsContainer = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'space-around',
+  width: '100%',
+  [theme.breakpoints.down('sm')]: {
+    flexDirection: 'column',
+  },
+}));
+
+const GridButtonsContainer = styled(Grid)(({ theme }) => ({
+
+}));
 
 function MovieInformation() {
+  const { id } = useParams();
+  const { data, isFetching, error } = useGetMovieQuery(id);
+  const dispatch = useDispatch();
+  const isMovieFavorited = false;
+  const isMovieWatchlisted = false;
+
+  const addToFavorites = () => {
+
+  };
+
+  const addToWatchlist = () => {
+
+  };
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <CircularProgress size="8rem" />
+      </Box>
+    );
+  }
+
+  if (error) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <Link to="/">Something has gone wrong - Go back</Link>
+      </Box>
+    );
+  }
+
   return (
-    <div>MovieInformation</div>
+    <GridContainerSpaceRound container>
+      <Grid item sm={12} lg={4} align="center">
+        <Poster
+          src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
+          alt={data?.title}
+        />
+      </Grid>
+      <Grid item container direction="column" lg={7}>
+        <Typography variant="h3" align="center" gutterBottom>
+          {data?.title} ({data.release_date.split('-')[0]})
+        </Typography>
+        <Typography variant="h5" align="center" gutterBottom>
+          {data?.tagline}
+        </Typography>
+        <GridContainerSpaceRound item>
+          <Box display="flex" align="center">
+            <Rating readOnly value={data.vote_average / 2} />
+            <Typography variant="subtitle1" gutterBottom sx={{ marginLeft: '10px' }}>
+              {data?.vote_average}
+            </Typography>
+          </Box>
+          <Typography variant="h6" align="center" gutterBottom>
+            {data?.runtime}min {data?.spoken_languages.length > 0 ? `/ ${data?.spoken_languages[0].name}` : ''}
+          </Typography>
+        </GridContainerSpaceRound>
+        <GenresContainer item>
+          {data?.genres?.map((genre) => (
+            <Links key={genre.name} to="/" onClick={() => dispatch(selectGenreOrCategory(genre.id))}>
+              <GenreImage
+                src={genreIcons[genre.name.toLowerCase()]}
+                height={30}
+              />
+              <Typography color="textPrimary" variant="subtitle1">
+                {genre?.name}
+              </Typography>
+            </Links>
+          ))}
+        </GenresContainer>
+        <Typography variant="h5" gutterBottom sx={{ marginTop: '10px' }}>
+          Overview
+        </Typography>
+        <Typography sx={{ marginBottom: '2rem' }}>
+          {data?.overview}
+        </Typography>
+        <Typography variant="h5" gutterBottom>Top Cast</Typography>
+        <Grid item container spacing={2}>
+          {data && data?.credits?.cast?.map((character, i) => (
+            character.profile_path
+            && (
+            <Grid key={i} item xs={4} md={2} component={Link} to={`/actors/${character.id}`} sx={{ textDecoration: 'none' }}>
+              <CastImage src={`https://image.tmdb.org/t/p/w500/${character.profile_path}`} alt={character.name} />
+              <Typography color="textPrimary">{character?.name}</Typography>
+              <Typography color="textSecondary">{character.character.split('/')[0]}</Typography>
+            </Grid>
+            )
+          )).slice(0, 6)}
+        </Grid>
+        <Grid item container sx={{ marginTop: '10px' }}>
+          <ButtonsContainer>
+            <GridButtonsContainer item xs={12} sm={6}>
+              <ButtonGroup size="medium" variant="outlined">
+                <Button target="_blank" rel="noopener noreferrer" href={data?.homepage} endIcon={<Language />}>Website</Button>
+                <Button target="_blank" rel="noopener noreferrer" href={`https://www.imdb.com/title/${data?.imdb_id}`} endIcon={<MovieIcon />}>IMDB</Button>
+                <Button onClick={() => {}} href="#" endIcon={<Theaters />}>Trailer</Button>
+              </ButtonGroup>
+            </GridButtonsContainer>
+            <GridButtonsContainer item xs={12} sm={6}>
+              <ButtonGroup size="medium" variant="outlined">
+                <Button onClick={addToFavorites} endIcon={isMovieFavorited ? <FavoriteBorderOutlined /> : <Favorite />}>
+                  {isMovieFavorited ? 'Unfavorite' : 'Favorite'}
+                </Button>
+                <Button onClick={addToWatchlist} endIcon={isMovieWatchlisted ? <Remove /> : <PlusOne />}>
+                  Watchlist
+                </Button>
+                <Button endIcon={<ArrowBack />} sx={{ borderColor: 'primary.main' }}>
+                  <Typography sx={{ textDecoration: 'none' }} component={Link} to="/" color="inherit" variant="subtitle2">
+                    Back
+                  </Typography>
+                </Button>
+              </ButtonGroup>
+            </GridButtonsContainer>
+          </ButtonsContainer>
+        </Grid>
+      </Grid>
+    </GridContainerSpaceRound>
   );
 }
 
